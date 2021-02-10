143ccf6dd5fffcb6694e6b3eef7bfd6b8a8edb30
chou <choukatanagari@yahoo.com>
2021-02-10 11:42:16 +0800

    second

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-# based on https://www.twilio.com/blog/2017/02/an-easy-way-to-read-and-write-to-a-google-spreadsheet-in-python.html
-# read that file for how to generate the creds and how to use gspread to read and write to the spreadsheet
-
-# use creds to create a client to interact with the Google Drive API
 scopes = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 json_creds = os.getenv("GOOGLE_SHEETS_CREDS_JSON")
 
@@ -16,10 +12,8 @@ creds_dict["private_key"] = creds_dict["private_key"].replace("\\\\n", "\n")
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scopes)
 client = gspread.authorize(creds)
 
-# Find a workbook by url
 spreadsheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1OQ8WUVFJJxKEkve0BvUi7t4G1K2d_RAB_YtE1chzKws")
 sheet = spreadsheet.sheet1
 
-# Extract and print all of the values
 rows = sheet.get_all_records()
 print(rows)
