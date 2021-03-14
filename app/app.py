ca76cb979cae8d5815a0f02d884c11ddd2bdf63a
chou <choukatanagari@yahoo.com>
2021-03-14 14:00:44 +0800

    new build

--- app/app.py
+++ app/app.py
@@ -10,8 +10,9 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 app = Flask(__name__)
 
+
 #live test
-scopes = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+scopes = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 json_creds = os.getenv("GOOGLE_SHEETS_CREDS_JSON")
 
 creds_dict = json.loads(json_creds)
@@ -19,16 +20,15 @@ creds_dict["private_key"] = creds_dict["private_key"].replace("\\\\n", "\n")
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scopes)
 client = gspread.authorize(creds)
 ##live test
-
 '''
 #local test
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name('ucc-pahang.json', scope)
 client = gspread.authorize(creds)
 ##local test
 '''
 
-spreadsheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1OQ8WUVFJJxKEkve0BvUi7t4G1K2d_RAB_YtE1chzKws")
+spreadsheet = client.open_by_url("https://docs.google.com/spreadsheets/d/14vFutdJuHcszQKIpAcqyQ106AtBSebeghrRxTp9ittQ")
 sheet = spreadsheet.sheet1
 currentdate = datetime.datetime.now()
 
@@ -38,11 +38,11 @@ def index():
     
     dict_sheet = sheet.get_all_records()
     currentdates = currentdate
-    x = currentdates.strftime("%d %B %Y")
+    update = currentdates.strftime("%d %B %Y")
 
     return render_template('index.html',  
     dict_sheet = dict_sheet,
-    x = x)
+    update = update)
 
 if __name__ == '__main__':
     app.run(debug=True)
